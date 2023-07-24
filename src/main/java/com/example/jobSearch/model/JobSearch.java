31d4e88997a578b5185ac6eed47e5695d55b652c
sanowar72 <atifreza72ar@gmail.com>
2023-07-24 16:25:15 +0530

    validation removed

--- src/main/java/com/example/jobSearch/model/JobSearch.java
+++ src/main/java/com/example/jobSearch/model/JobSearch.java
@@ -1,10 +1,10 @@
 package com.example.jobSearch.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+// import jakarta.persistence.*;
+// import jakarta.validation.constraints.Email;
+// import jakarta.validation.constraints.Min;
+// import jakarta.validation.constraints.NotBlank;
+// import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,17 +16,17 @@ import lombok.NoArgsConstructor;
 @Table(name="job")
 public class JobSearch {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long jobId;
     private String jobTitle;
     private String jobDesc;
     private String jobLocation;
-    @Min(value = 20000, message = "Minimum salary should be 20,000")
+    // @Min(value = 20000, message = "Minimum salary should be 20,000")
     private Double jobSalary;
-    @Email(message = "Invalid email format")
-    @NotNull(message = "Company email is required")
+    // @Email(message = "Invalid email format")
+    // @NotNull(message = "Company email is required")
     private String companyEmail;
-    @NotBlank
+    // @NotBlank
     private String companyName;
     private String employerName;
     @Enumerated(EnumType.STRING)
